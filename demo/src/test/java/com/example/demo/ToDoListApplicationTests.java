61f9ae28cdc452fe831228aabf12655612875bf7
Yerdaulet <yerdauletacc@gmail.com>
2026-02-05 22:26:52 +0500

    Project test case

--- demo/src/test/java/com/example/demo/ToDoListApplicationTests.java
+++ demo/src/test/java/com/example/demo/ToDoListApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.jira.TaskManagerApplication;
+import com.example.demo.jira.project.dto.ProjectResponse;
 import com.example.demo.jira.user.Dto.UserResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.checkerframework.checker.formatter.qual.UnknownFormat;
@@ -64,5 +65,76 @@ class ToDoListApplicationTests {
 		mockMvc.perform(delete("/users/"+userId))
 				.andExpect(status().isNoContent());
 	}
+
+	@Test
+	void projectLifeCycle() throws Exception {
+
+		String userJson = """
+			{
+				"name":"Yerdaulet"
+			}
+			""";
+
+		MvcResult creation = mockMvc.perform(post("/users")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(userJson))
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.name").value("Yerdaulet"))
+				.andReturn();
+
+		String respondUser = creation.getResponse().getContentAsString();
+
+		UserResponse createdUser = mapper.readValue(respondUser, UserResponse.class);
+		Long userId = createdUser.id();
+
+		String userName = createdUser.name();
+
+		String projectJson = """
+			{
+				"name":"todo List"
+			}
+			""";
+
+		MvcResult projectCreate = mockMvc.perform(post("/users/"+userId+"/projects")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(projectJson))
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.name").value("todo List"))
+				.andReturn();
+
+		String respondProject = projectCreate.getResponse().getContentAsString();
+
+
+		ProjectResponse createdProject = mapper.readValue(respondProject, ProjectResponse.class);
+		Long projectId = createdProject.id();
+		String projectName = createdProject.name();
+
+		String projectUpdateJson = """
+			{
+				"name":"Task Manager"
+			}
+			""";
+
+		mockMvc.perform(get("/users/" +userId+"/projects/"+projectId))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.owner").value(userName))
+				.andExpect(jsonPath("$.name").value(projectName));
+
+
+
+		mockMvc.perform(put("/users/" +userId+"/projects/"+projectId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(projectUpdateJson))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("Task Manager"));
+
+		mockMvc.perform(delete("/users/" +userId+"/projects/"+projectId))
+				.andExpect(status().isNoContent());
+
+		mockMvc.perform(delete("/users/" +userId))
+				.andExpect(status().isNoContent());
+	}
+
+
 
 }
